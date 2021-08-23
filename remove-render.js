7d5c26d14734c7635c5e0c9ca7f03f5201f8d302
Nishant Pandey <59759332+nishant2106@users.noreply.github.com>
2021-08-23 15:59:28 +0530

    Updated work around code
    To access the return statement inside render() function we have used its range but it will not work in all the cases and for different formats.

--- remove-render.js
+++ remove-render.js
@@ -1,10 +1,17 @@
 export default function transformer(file, api) {
+  const fs = file.source  // file as a string
+
   const j = api.jscodeshift;
-  const renderExpression = j(file.source).find(j.ClassProperty)
-  console.log(renderExpression)
-  return j(file.source)
-    .find(j.MethodDefinition)
-    .remove()
-    // .replaceWith(j)
-    .toSource();
+	const ret = j(file.source).find(j.ReturnStatement)
+  if(ret){
+    const start = ret.__paths[0].value.start
+    const end = ret.__paths[0].value.end
+  
+    return j(file.source)
+    .find(j.MethodDefinition,{
+      key:{type:"Identifier",name:"render"}
+    }) 
+    .replaceWith(fs.slice(start,end+1))
+    .toSource()
+  }
 }
